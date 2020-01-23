3589f48ced3689f9aa58861073be0a1fe630b036
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-01-23 19:06:46 +0000

    adding wait functionality and check on to frequent dns updates

--- check-and-update.go
+++ check-and-update.go
@@ -10,6 +10,12 @@ import (
 	"net/http"
 	"os"
 	"sort"
+	"time"
+)
+
+const (
+	// format generated from https://golang.org/src/time/format.go
+	layoutCF = "2006-01-02 15:04:05.000000 -0700 MST"
 )
 
 var (
@@ -22,8 +28,10 @@ var (
 
 func init() {
 	flag.Bool("displayconfig", false, "Display configuration")
+	flag.Bool("updatedns", false, "Update DNS")
 	flag.String("domain", "narco.tk", "DNS Domain, default = narco.tk")
 	flag.String("host", "test1", "Hostname, default = test1")
+	flag.Int("wait", 300, "Seconds to wait since last modificaiton")
 	viper.SetEnvPrefix("CF")
 	viper.BindEnv("API_EMAIL")
 	viper.BindEnv("API_KEY")
@@ -90,20 +98,38 @@ func main() {
 		fmt.Printf("%s: %s %s %d %s/%s\n", r.Name, r.Type, r.Content, r.TTL, r.CreatedOn, r.ModifiedOn)
 		fmt.Printf("last modified: %s\n", r.ModifiedOn)
 		// temportarily over ride dns record to be the real one for my home
-		newdnsrecord.Content = "82.34.44.205"
+		//newdnsrecord.Content = "82.34.44.205"
 
 		if r.Content == newdnsrecord.Content {
 			fmt.Println("current = new: ignoring")
 		} else {
 			fmt.Println("needs updating")
 
-			recs, err := api.CreateDNSRecord(zoneID, newdnsrecord)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+			lastmodified, _ := time.Parse(layoutCF, r.ModifiedOn.String())
+			timenow := time.Now().UTC()
+			timediff := timenow.Sub(lastmodified).Round(time.Second).Seconds()
 
-			fmt.Println(recs)
+			fmt.Println("       now:", timenow)
+			fmt.Println("  modified:", lastmodified)
+			fmt.Println("difference:", timediff)
+			fmt.Println("      wait:", viper.GetInt("wait"))
+
+			if int64(timediff) >= int64(viper.GetInt("wait")) {
+				fmt.Printf("updating dns because it was last updated more than %d seconds ago and wait time set to %d seconds\n", int64(timediff), int64(viper.GetInt("wait")))
+				if viper.GetBool("updatedns") {
+					recs, err := api.CreateDNSRecord(zoneID, newdnsrecord)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					fmt.Println(recs)
+				} else {
+					fmt.Println("updatedns=false")
+				}
+
+			} else {
+				fmt.Printf("not updating dns as it was only updated %d seconds ago\n", int64(timediff))
+			}
 
 		}
 
