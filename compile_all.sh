43223bc7c65eb25cf4b76cc842bbe2c313d59f31
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-02-11 12:44:21 +0000

    Adding compile script

---
+++ compile_all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apps=( narcotk-cf-ddns )
+
+set -x
+
+for i in "${apps[@]}"
+do
+  GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o binaries/x86_64/$i $i.go; upx binaries/x86_64/$i
+  GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w" -o binaries/rpi/$i $i.go; upx binaries/rpi/$i
+  GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o binaries/osx/$i $i.go; upx binaries/osx/$i
+done
